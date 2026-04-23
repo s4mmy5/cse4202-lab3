b1b7f594654832a9be7c245f7788b50aa0d55c70
Jonathan Rodriguez-Gomez <j.rodriguezgomez@wustl.edu>
2026-04-22 23:10:33 -0500

    fix fragment reading, add foundation for epoll

--- src/server.c
+++ src/server.c
@@ -1,28 +1,34 @@
-#include <cstdlib>
 #include <err.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #define LISTEN_BACKLOG 50
 
 int init_socket(void);
-void usage(void);
+int usage(void);
+void set_non_blocking_io(int fd);
 
 int main(int argc, char *argv[]) {
   // 1. Open all required files
   FILE *in_file, *out_file;
-  FILE **fragments;
-  int fragments_cnt;
-  char *line;
-  size_t line_len;
-  int sfd;
+  int sfd, epoll_fd, ret, ready;
+  FILE **fragments = NULL;
+  int fragments_cnt = 0;
+  char *line = NULL;
+  char *fragment_filename = NULL;
+  size_t line_len = 0;
+  struct epoll_event ev = NULL;
+  struct epoll_event *revents = NULL;
+  int epollfd_count = 0;
 
   if (argc != 2)
-    usage();
+    return usage();
 
   if (NULL == (in_file = fopen(argv[1], "r"))) {
     printf("Could not open input file\n");
@@ -31,23 +37,59 @@ int main(int argc, char *argv[]) {
   }
 
   if (-1 == getline(&line, &line_len, in_file)) // get output file name
-    {
-      printf("Input file is emtpy\n");
-      err(EXIT_FAILURE, "getline");
+  {
+    printf("Input file is emtpy\n");
+    err(EXIT_FAILURE, "getline");
   }
 
-  if (NULL == (out_file = fopen(line, "w"))) // open output file and truncate old contents
+  if (NULL == (out_file = fopen(
+                   line, "w"))) // open output file and truncate old contents
   {
     printf("Could not open output file");
     err(EXIT_FAILURE, "fopen");
   }
 
   while (-1 != getline(&line, &line_len, in_file)) {
-    fragments = realloc(fragments,
+    fragments = realloc(fragments, (++fragments_cnt) * sizeof(FILE *));
+
+    if (NULL == fragments) {
+      printf("Could not allocate fragments array\n");
+      err(EXIT_FAILURE, "realloc");
+    }
+
+    line[strcspn(line, "\n")] = '\0';
+    fragments[fragments_cnt - 1] = fopen(line, "r");
+
+    if (NULL == fragments[fragments_cnt - 1]) {
+      printf("Could not open fragment file %d\n", fragments_cnt);
+      err(EXIT_FAILURE, "fopen");
+    }
+  }
+
+  // initialize epoll fd
+  epoll_fd = epoll_create1(0);
+  if (-1 == epoll_fd) {
+    err(EXIT_FAILURE, "epoll_create1");
   }
 
   // 2. Connect with all clients
   sfd = init_socket();
+
+  // initialize socket epoll_event
+  ev.events = EPOLLIN;
+  ev.data.fd = sfd;
+  if (-1 == epoll_ctl(epoll_fd, EPOLL_CTL_ADD, STDIN_FILENO, &ev)) {
+    err(EXIT_FAILURE, "epoll_ctl");
+  }
+
+  revents = realloc(revents, sizeof(struct epoll_event) * ++epollfd_count);
+
+  // set non blocking readers
+  set_non_blocking_io(sfd);
+
+  // wait for connections
+  while ((ready = epoll_wait(epoll_fd, revents, epollfd_count, -1)) != -1) {
+  }
 }
 
 int init_socket(void) {
@@ -72,7 +114,7 @@ int init_socket(void) {
   getsockname(sfd, (struct sockaddr *)&my_addr, &sfd_len);
 
   getnameinfo((struct sockaddr *)&my_addr, sfd_len, server_host, NI_MAXHOST,
-              server_serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+              NULL, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
 
   printf("Server Host: %s\n"
          "Server Port: %s\n",
@@ -81,7 +123,16 @@ int init_socket(void) {
   return sfd;
 }
 
-void usage(void) {
+int usage(void) {
   printf("./server <input_file>"
          "input_file: Provide a file containing fragments.");
+  return EXIT_FAILURE;
+}
+
+void set_non_blocking_io(int fd) {
+  int flags;
+
+  flags = fcntl(fd, F_GETFL, 0);
+  flags |= O_NONBLOCK;
+  fcntl(fd, F_SETFL, flags);
 }
